b4a381a978db423533cd96123ed18c27a7889c5c
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2026-02-23 10:54:59 +0700

    implement auth with zustand

--- 18-feb-2026/todo-app/src/App.tsx
+++ 18-feb-2026/todo-app/src/App.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import Todos from "./pages/todos"
+import Login from "./pages/login"
+
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/todos" element={<Todos />} />
+      <Route path="/todos" element={
+        <PrivateRoute>
+          <Todos />
+        </PrivateRoute>
+      } />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
